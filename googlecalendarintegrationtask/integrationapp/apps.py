d6bbc8b1222a9f4895555a3dc06f0f99ba9158ba
Vignesh-PyDev <vigneshwarann@sase.ssn.edu.in>
2023-04-25 11:06:18 +0530

    Changed credentials,

--- googlecalendarintegrationtask/integrationapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IntegrationappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'integrationapp'
--- googlecalendarintegrationtask/integrationapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('v1/calendar/init/', views.GoogleCalendarInitView, name='google_permission'),
-    path('v1/calendar/redirect/', views.GoogleCalendarRedirectView, name='google_redirect'),
-
-]
